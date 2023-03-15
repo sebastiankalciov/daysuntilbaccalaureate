57687d3b6c44b1ae3f6a31f3e7d0638730eb981a
Sebastian Kalciov <66016734+ducktheduck@users.noreply.github.com>
2023-03-15 17:04:01 +0200

    Update countdown.js

--- source/functions/countdown.js
+++ source/functions/countdown.js
@@ -2,27 +2,21 @@ function Counter() {
 
     var x = setInterval(function() {
 
-        // Declaring a variable for exam date
+
         var finalDay = new Date ("Jun 26, 2023, 09:00:00")
-        // Declaring a variable with today's date
         var today = new Date().getTime();
-
-        // Time between two dates
         var distance = finalDay - today;
-
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Values assigned for each variable (days, hours, minutes, seconds)
-      
         document.getElementById("days").innerHTML = days
         document.getElementById("hours").innerHTML = hours
         document.getElementById("minutes").innerHTML = minutes
         document.getElementById("seconds").innerHTML = seconds
 
-        // Exception case. When the exam date is reached, all variables are set to 0
+        // Exception case.
         if (distance < 0) {
 
             clearInterval(x);
